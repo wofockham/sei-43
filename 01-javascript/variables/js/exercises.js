4d4fafa490b4f043abf6c8897ee027ad156dcaae
JT <certainthings@gmail.com>
2021-03-23 14:22:14 +1100

    variable exercises ii

--- 01-javascript/variables/js/exercises.js
+++ 01-javascript/variables/js/exercises.js
@@ -47,7 +47,17 @@ console.log(ageMessage);
 // - Store an estimated amount per day (as a number).
 // - Calculate how many you would eat total for the rest of your life.
 // - Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
-//
+
+const currentAge = 36;
+const deathAge = 84;
+const amountPerDay = 11; // cheesecakes
+
+const amountPerYear = amountPerDay * 365.25; // leap years
+const yearsRemaining = deathAge - currentAge;
+
+const amountRequired = amountPerYear * yearsRemaining;
+console.log(`You will need ${ amountRequired } to last you until the ripe old age of ${ deathAge }`);
+
 // ## The Geometrizer
 //
 // Calculate properties of a circle, using [the definitions here](http://math2.org/math/geometry/circles.htm).
@@ -55,7 +65,13 @@ console.log(ageMessage);
 // - Store a radius into a variable.
 // - Calculate the circumference based on the radius, and output "The circumference is NN".
 // - Calculate the area based on the radius, and output "The area is NN".
-//
+
+const radius = 1;
+const circumference = 2 * radius * Math.PI; // diameter * PI
+console.log(`The circumference is ${ circumference }`);
+const area = Math.PI * radius * radius; // PI * r ^ 2
+console.log(`The area is ${ area }`);
+
 // ## The Temperature Converter
 //
 // It's hot out! Let's make a converter based on [the steps here](http://www.mathsisfun.com/temperature-conversion.html).
@@ -64,3 +80,10 @@ console.log(ageMessage);
 // - Convert it to fahrenheit and output "NN°C is NN°F".
 // - Now store a fahrenheit temperature into a variable.
 // - Convert it to celsius and output "NN°F is NN°C."
+
+const currentTemperature = 18;
+const fahrenheit = currentTemperature * 1.8 + 32;
+console.log(`${ currentTemperature }°C is ${ fahrenheit }°F`);
+
+const celsius = (fahrenheit - 32) / 1.8;
+console.log(`${ fahrenheit }°F is ${ celsius }°C`);
