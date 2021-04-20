3ed4b6356089d6263114c1e5549da9fd7976a683
JT <certainthings@gmail.com>
2021-04-20 16:07:03 +1000

    view helpers ii

--- 08-rails/view-helpers/config/routes.rb
+++ 08-rails/view-helpers/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
-  get '/numbers' => 'pages#numbers'
+  get '/kfc-numbers' => 'pages#numbers', :as => :numbers # Rename the new path to the old name: numbers_path
   get '/text' => 'pages#text'
   get '/assets' => 'pages#assets'
   get '/url' => 'pages#url'
