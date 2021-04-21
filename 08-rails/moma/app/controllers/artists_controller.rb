ff0af1a654356065da5106703f9a28d0b31bcf34
JT <certainthings@gmail.com>
2021-04-21 12:00:31 +1000

    new and create

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -6,4 +6,19 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find params[:id]
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.create artist_params
+    redirect_to artist # show page
+  end
+
+  # Strong params
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :nationality, :dob, :period, :image)
+  end
 end
