7294e7da964ac817b97f851ff9d1c080383db850
JT <certainthings@gmail.com>
2021-04-23 09:45:49 +1000

    sign in on sign up

--- 08-rails/tunr/app/controllers/users_controller.rb
+++ 08-rails/tunr/app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save # returns truthy on success
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
