cf82445b63720fc9bcc3d0e7704ef4fa09240e9f
JT <certainthings@gmail.com>
2021-05-06 10:33:23 +1000

    flickr search with manual choking

--- 10-ajax/image-search/js/search.js
+++ 10-ajax/image-search/js/search.js
@@ -1,15 +1,34 @@
+const state = {
+  nextPage: 1,
+  lastPageReached: false,
+  requestInProgress: false
+};
+
 const searchFlickr = function (keywords) {
+
+  if (state.lastPageReached || state.requestInProgress) {
+    return; // Bail out.
+  }
+
   console.log('Searching for', keywords);
+
+  state.requestInProgress = true;
 
   const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?'; // JSONP
   $.getJSON(flickrURL, {
     method: 'flickr.photos.search', // not to be confused with HTTP methods like GET/POST
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: keywords,
-    format: 'json'
+    format: 'json',
+    page: state.nextPage++
   }).done(showImages).done(function (info) {
     console.log(info);
+    if (info.photos.page >= info.photos.pages) {
+      state.lastPageReached = true;
+    }
+    state.requestInProgress = false;
   });
+
 };
 
 const showImages = function (results) {
@@ -39,6 +58,8 @@ $(document).ready(function () {
     event.preventDefault(); // disable the form being submitted to the server.
 
     $('#images').empty();
+    state.nextPage = 1;
+    state.lastPageReached = false;
 
     const searchTerms = $('#query').val();
     searchFlickr(searchTerms);
@@ -47,7 +68,6 @@ $(document).ready(function () {
   // Very twitchy
   $(window).on('scroll', function () {
     const scrollBottom = $(document).height() - $(window).scrollTop() - $(window).height();
-    console.log(scrollBottom);
 
     if (scrollBottom < 400) {
       const searchTerms = $('#query').val();
