79744d17773c4053c969364f86535d178fd0fa98
JT <certainthings@gmail.com>
2021-05-07 15:08:37 +1000

    <SocialNetwork />

--- 13-react/social-network/src/components/App.js
+++ 13-react/social-network/src/components/App.js
@@ -6,6 +6,8 @@ function App() {
       <h1>Social Network</h1>
 
       <Profile name="Groucho Marx" age="45" motto="No gods no monsters" pic="http://www.fillmurray.com/500/699" />
+      <Profile name="Harpo Marx" age="44" pic="http://www.fillmurray.com/501/699" />
+      <Profile name="Chico Marx" age="46" motto="No gods no monsters" pic="http://www.fillmurray.com/502/699" />
 
     </div>
   );
--- 13-react/social-network/src/components/Profile.js
+++ 13-react/social-network/src/components/Profile.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
 class Profile extends Component {
+  constructor() { // setup
+    super();
+
+    this.state = {
+      height: 100 // initial value
+    };
+
+    this._zoomIn = this._zoomIn.bind(this); // This makes it work.
+    this._zoomOut = this._zoomOut.bind(this); // This makes it work.
+  }
+
+  _zoomIn() {
+    this.setState({height: this.state.height + 10});
+  }
+
+  _zoomOut() {
+    // height cannot go below zero:
+    if (this.state.height - 10 > 0) {
+      this.setState({height: this.state.height - 10});
+    }
+  }
+
   render() {
     const { name, age, motto, pic } = this.props; // Destructuring.
 
@@ -9,7 +31,11 @@ class Profile extends Component {
         <h2>{ name }</h2>
         <h3>Age: { age }</h3>
         <p>{ motto }</p>
-        <img src={ pic } alt={ name } />
+        <div>
+          <button onClick={ this._zoomIn }> + </button>
+          <button onClick={ this._zoomOut }> - </button>
+        </div>
+        <img src={ pic } alt={ name } height={ this.state.height } />
       </div>
     );
   }
