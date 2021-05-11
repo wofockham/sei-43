b670e567a4d7dc62285df521acf5da1a6cbdca63
JT <certainthings@gmail.com>
2021-05-11 12:43:53 +1000

    pre-AJAX secrets client

---
+++ 13-react/secrets-client/src/components/App.js
@@ -0,0 +1,11 @@
+import Secrets from './Secrets';
+
+function App() {
+  return (
+    <div className="App">
+      <Secrets />
+    </div>
+  );
+}
+
+export default App;
---
+++ 13-react/secrets-client/src/components/Secrets.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const SERVER_URL = 'http://localhost:3000/secrets.json';
+
+class Secrets extends Component {
+  constructor() {
+    super();
+    // TODO: fetch these via AJAX
+    this.state = {
+      secrets: []
+    };
+
+    axios.get(SERVER_URL).then((results) => {
+      console.log( results.data ); // WE ARE NEARLY FINISHED!
+    });
+
+    this.saveSecret = this.saveSecret.bind(this);
+  }
+
+  saveSecret(content) {
+    // TODO: save new secrets via AJAX
+    const secret = {
+      id: Math.random(), // TODO: get an actual ID from the database
+      content: content
+    };
+
+    // Add a new secret to the collection of secrets without mutation.
+    this.setState({ secrets: [...this.state.secrets, secret]});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Tell me all your secrets</h1>
+        <SecretForm onSubmit={ this.saveSecret } />
+        <SecretsList secrets={ this.state.secrets } />
+      </div>
+    );
+  }
+}
+
+class SecretForm extends Component {
+  constructor() {
+    super();
+    this.state = { content: '' };
+    this._handleChange = this._handleChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleChange(event) {
+    this.setState({ content: event.target.value });
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.content);
+    this.setState({content: ''}); // Reset the form
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this._handleSubmit }>
+        <textarea onChange={ this._handleChange } value={ this.state.content } />
+        <input type="submit" value="Tell" />
+      </form>
+    );
+  }
+}
+
+const SecretsList = (props) => {
+  return (
+    <div>
+      { props.secrets.map((s) => <p key={ s.id }>{ s.content }</p>) }
+    </div>
+  );
+}
+
+export default Secrets;
